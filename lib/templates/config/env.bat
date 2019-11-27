9e2d7d385eb562b6906fac0364d212036ba880ca
maka-io <mjmcee@gmail.com>
2019-11-27 10:04:21 -0500

    fixes some loooooooong standing environment settings issues

--- lib/templates/config/env.bat
+++ lib/templates/config/env.bat
@@ -1,9 +1,9 @@
-# This .bat file will be sourced before starting your application.
-# You can use it to put environment variables you want accessible
-# to the server side of your app by using process.env.MY_VAR
-#
-# Example:
+:: This .bat file will be sourced before starting your application.
+:: You can use it to put environment variables you want accessible
+:: to the server side of your app by using process.env.MY_VAR
+::
+:: Example:
 
-# setx MAIL_URL="smtp://gmail.com:25"
-# setx MONGO_URL="mongodb://localhost:27017/<%= app %>"
-# setx ROOT_URL="http://localhost:3000"
+:: set MAIL_URL="smtp://gmail.com:25"
+:: set MONGO_URL="mongodb://localhost:27017/<%= app %>"
+:: set ROOT_URL="http://localhost:3000"
