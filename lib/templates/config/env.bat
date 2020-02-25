25732d0f529e12c4cfc0ffc39e1696826ea9f0c8
maka-io <mjmcee@gmail.com>
2020-02-25 10:37:48 -0500

    help comment in env.bat if you have problems starting your app on windows if you did not use choco

--- lib/templates/config/env.bat
+++ lib/templates/config/env.bat
@@ -7,3 +7,9 @@
 :: set MAIL_URL="smtp://gmail.com:25"
 :: set MONGO_URL="mongodb://localhost:27017/<%= app %>"
 :: set ROOT_URL="http://localhost:3000"
+::
+::
+:: If you are running into problems when starting your app on Windows
+:: in reference to https://forums.meteor.com/t/meteor-stuck-at-starting-your-app/25592/28
+:: then uncomment this line below
+:: del app/.meteor/local/db/METEOR-PORT
