c652d99ab1d61fb14b0e2e978782f146540b5935
maka-io <mjmcee@gmail.com>
2020-03-18 12:55:05 -0400

    fix forward to back slash

--- lib/templates/config/env.bat
+++ lib/templates/config/env.bat
@@ -12,4 +12,4 @@
 :: If you are running into problems when starting your app on Windows
 :: in reference to https://forums.meteor.com/t/meteor-stuck-at-starting-your-app/25592/28
 :: then uncomment this line below
-:: del app/.meteor/local/db/METEOR-PORT
+:: del app\.meteor\local\db\METEOR-PORT
